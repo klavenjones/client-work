ebe8bd58d8093e96e87f8052f9ad8733702b3858
Klaven Jones <klavensjones@gmail.com>
2021-06-20 16:11:24 -0400

    Added User Modal

--- pages/api/examples/protected.js
+++ pages/api/examples/protected.js
@@ -1,6 +1,6 @@
 // This is an example of to protect an API route
 import { getSession } from 'next-auth/client'
-
+import User from '../../../models/Users'
 import dbConnect from '../../../util/dbConnection'
 
 export default async (req, res) => {
@@ -8,17 +8,14 @@ export default async (req, res) => {
 
   await dbConnect()
 
-  
-
-
   if (session) {
-    res.send({
-      content:
-        'This is protected content. You can access this content because you are signed in.'
-    })
+    const users = await User.find({}) /* find all the data in our database */
+    res.status(200).json({ success: true, data: users })
+    // res.send({ content: 'Hello' })
   } else {
     res.send({
       error: 'You must be sign in to view the protected content on this page.'
     })
   }
+  ;``
 }
